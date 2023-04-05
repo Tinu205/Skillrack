f4e5b63199d2dc392475351cc9e031c5e55a1cbf
Tinu205 <dazleroger404@gmail.com>
2023-04-05 16:42:03 +0530

    Reverse alphabet triangle patter.py

---
+++ Programming Tracks/Kickstart003/Reverse alphabet triangle patter.py
@@ -0,0 +1,9 @@
+n = int(input())
+alpha = ord('a')
+for i in range(1,n+1):
+    print("-"*(n-i),end="")
+    for j in range(1,i+1):
+        print(chr(alpha),end="")
+        alpha-=1
+    alpha+=(i+1)
+    print()
