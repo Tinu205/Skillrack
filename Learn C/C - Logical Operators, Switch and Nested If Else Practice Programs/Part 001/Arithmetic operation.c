853a1369fb5b4d5c23419c2b3446fcb65a5c41da
Tinu205 <dazleroger404@gmail.com>
2023-04-17 17:02:06 +0530

    Arithmetic operation.c

---
+++ Learn C/C - Logical Operators, Switch and Nested If Else Practice Programs/Part 001/Arithmetic operation.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+//sum if both are even
+//product if both or odd
+//abs diff else
+
+int main(){
+    int num1,num2;
+    scanf("%d%d",&num1,&num2);
+    if((num1%2==0)&&(num2%2==0)){
+        printf("%d",num1+num2);
+    }else if((num1%2!=0)&&(num2%2 != 0)){
+        printf("%d",num1*num2);
+    }else{
+        printf("%d",abs(num1-num2));
+    }
+}
+//incomplete
