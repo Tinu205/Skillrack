e7f778d5999cc3e5a3f6312f79e8632d37ea9f47
Tinu205 <dazleroger404@gmail.com>
2023-05-20 21:01:26 +0530

    Days in a month.c

---
+++ Learn C/C - Logical Operators, Switch and Nested If Else Practice Programs/Part 002/Days in a month.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int leap_year(int value){
+
+}
+int month_resutl(int mo[],int val){
+    for(int i =0;i<sizeof(mo);i++){
+        if (mo[i] = val){
+            return 1;
+            break;
+        }else{
+            return 0;
+        }
+    }
+}
+int main(){
+    int day, year;
+    scanf("%d%d",&day,&year);
+    int o_month []= {1,3,5,7,9,11},e_month[] = {4,6,8,10,12};
+    
+}
