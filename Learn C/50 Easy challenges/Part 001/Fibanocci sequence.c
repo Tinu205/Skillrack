07073da83cbaa4eb9ca2d3d04485086d40a2cf26
Tinu205 <dazleroger404@gmail.com>
2023-06-14 19:45:20 +0530

    Fibanocci sequence.c

--- Learn C/50 Easy challenges/Part 001/Fibanocci sequence.c
+++ Learn C/50 Easy challenges/Part 001/Fibanocci sequence.c
@@ -1,9 +1,20 @@
 #include<stdio.h>
 
-int main(){
-    int val,curr = 0,previ = 0;
-    scanf("%d",&val);
-    for( int i=0; i<val; i++){
+int main() {
+    int val;
+    scanf("%d", &val);
 
+    int first = 0;
+    int second = 1;
+
+    //printf("Fibonacci Sequence: ");
+
+    for (int i = 0; i < val; i++) {
+        printf("%d ", first);
+        int next = first + second;
+        first = second;
+        second = next;
     }
+
+    return 0;
 }
