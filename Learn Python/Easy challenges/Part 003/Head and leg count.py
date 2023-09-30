3c98b53f67e72c8196cf3ab9d2a5332dc77e6dcc
Tinu205 <dazleroger404@gmail.com>
2023-09-30 17:52:14 +0530

    Added features

---
+++ Learn Python/Easy challenges/Part 003/Head and leg count.py
@@ -0,0 +1,8 @@
+head_count = int(input())
+leg_count = int(input())
+
+bird_count = (leg_count - head_count) / 2
+animal_count = head_count - bird_count
+
+print(bird_count,animal_count)
+#incomplete
